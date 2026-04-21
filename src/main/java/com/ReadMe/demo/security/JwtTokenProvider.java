33ec47c73846d09476c3626673946ebba6d707fd
16eunho <16eunho@16eunhoui-MacBookPro.local>
2026-04-21 22:53:07 +0900

    accessToken

--- src/main/java/com/ReadMe/demo/security/JwtTokenProvider.java
+++ src/main/java/com/ReadMe/demo/security/JwtTokenProvider.java
@@ -4,10 +4,13 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 // JwtTokenProvider.java
@@ -26,12 +29,13 @@ public class JwtTokenProvider {
     public String generateAccessToken(String userId) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + ACCESS_TOKEN_VALIDITY);
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
 
         return Jwts.builder()
                 .setSubject(userId)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
-                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .compact();
     }
 
